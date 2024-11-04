3de920901f14eddb0545a2d1591eeeab25bfc1bb
Elias Feliciano <eliaszarrouk@gmail.com>
2024-11-04 17:06:46 +0000

    Added the possibility of arguments on the program (like ./flatpak_installer.sh flathub org.gnome.Nautilus)

--- flatpak_installer.sh
+++ flatpak_installer.sh
@@ -22,24 +22,27 @@ echo ""
 echo ""
 echo ""
 
+if [ $# != 2 ]; then
+  #Questions for the program to work
+  if [[ $LANG = "pt_PT" || $LANG = "pt_PT.UTF-8" || $LANG = "pt_BR" || $LANG = "pt_BR.UTF-8" ]]; then
+    echo -e "${YELLOW}Em que repositorio esta o teu pacote ?${NC}"
+  else
+    echo -e "${YELLOW}In what repository is your package ?${NC}"
+  fi
+  read rep
+  if [[ $LANG = "pt_PT" || $LANG = "pt_PT.UTF-8" || $LANG = "pt_BR" || $LANG = "pt_BR.UTF-8" ]]; then
+    echo -e "${YELLOW}Qual pacote queres instalar pelo "$rep" ?${NC}"
+  else
+    echo -e "${YELLOW}What package do you want to install from "$rep" ?${NC}"
+  fi
+  read package
 
-#Questions for the program to work
-if [[ $LANG = "pt_PT" || $LANG = "pt_PT.UTF-8" || $LANG = "pt_BR" || $LANG = "pt_BR.UTF-8" ]]; then
-        echo -e "${YELLOW}Em que repositorio esta o teu pacote ?${NC}"
+  echo ''
+  echo ''
 else
-	echo -e "${YELLOW}In what repository is your package ?${NC}"
+  rep="$1"
+  package="$2"
 fi
-read rep
-if [[ $LANG = "pt_PT" || $LANG = "pt_PT.UTF-8" || $LANG = "pt_BR" || $LANG = "pt_BR.UTF-8" ]]; then
-	echo -e "${YELLOW}Qual pacote queres instalar pelo "$rep" ?${NC}"
-else
-	echo -e "${YELLOW}What package do you want to install from "$rep" ?${NC}"
-fi
-read package
-
-echo ''
-echo ''
-
 #Installation of the flatpak
 if [[ $LANG = "pt_PT" || $LANG = "pt_PT.UTF-8" || $LANG = "pt_BR" || $LANG = "pt_BR.UTF-8" ]]; then
         echo -e "${GREEN}Instalando${NC}"
@@ -58,9 +61,9 @@ if [ -f "$(flatpak info --show-location $package)/files/share/applications/$pack
 	sudo cp $(flatpak info --show-location $package)/files/share/applications/$package.desktop /usr/share/applications/$package.desktop
 else
 	if [[ $LANG = "pt_PT" || $LANG = "pt_PT.UTF-8" || $LANG = "pt_BR" || $LANG = "pt_BR.UTF-8" ]]; then
-		echo "${RED}[ERRO] .desktop do Flatpak nao encontrado${NC}"
+		echo -e "${RED}[ERRO] .desktop do Flatpak nao encontrado${NC}"
 	else	
-		echo "${RED}[ERROR] Flatpak's .desktop not found${NC}"
+		echo -e "${RED}[ERROR] Flatpak's .desktop not found${NC}"
 	fi
 fi
 
